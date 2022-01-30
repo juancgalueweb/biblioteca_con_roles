8bdc4f4926d6aa2bd08601796e6185f244fe3420
juancgalueweb <juancgalue@icloud.com>
2022-01-29 21:23:59 -0300

    feat: :sparkles: Verificar email al registrar generando token de 4 dígitos

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,6 +1,11 @@
-const { registerUser, loginUser } = require("../controllers/user.controllers");
+const {
+  registerUser,
+  loginUser,
+  verifyEmail,
+} = require("../controllers/user.controllers");
 
 module.exports = (app) => {
   app.post("/api/auth/register", registerUser);
   app.post("/api/auth/login", loginUser);
+  app.post("/api/auth/verify-email", verifyEmail);
 };
