941d21fb91a37609ff0b1726ab5dc0ecd00a45fa
juancgalueweb <juancgalue@icloud.com>
2022-11-16 02:22:08 -0300

    Adds some comments

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const local_db = process.env.LOCAL_DB_URL; //To manage DB locally
-const deploy_db = process.env.MONGO_URI; //To manage DB with MongoDB Atlas
+const deploy_db = process.env.MONGO_URI; //To manage DB with MongoDB Atlas !
 
 main().catch((err) => console.log(err));
 async function main() {
