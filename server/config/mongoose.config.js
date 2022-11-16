391ee437038568cdd2a1dc6d7adb031716aada1a
juancgalueweb <juancgalue@icloud.com>
2022-11-16 02:10:47 -0300

    Adds mongo-uri to handle DB with MongoDB Atlas

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
 
+const local_db = process.env.LOCAL_DB_URL;
+const deploy_db = process.env.MONGO_URI;
+
 main().catch((err) => console.log(err));
 async function main() {
-  await mongoose.connect(process.env.LOCAL_DB_URL);
+  await mongoose.connect(deploy_db);
 }
